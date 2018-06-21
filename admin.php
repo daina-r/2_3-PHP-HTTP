434c2848fb525ad9926033e6e70e2ecc6d7cdd53
daina-r <36935561+daina-r@users.noreply.github.com>
2018-06-21 19:50:15 +0300

    Add files via upload

--- admin.php
+++ admin.php
@@ -1,6 +1,19 @@
 <?php
+
 $dir = 'tests';
 $files = array_diff( scandir( $dir), array('..', '.'));
+
+if (!empty($_FILES["json"])) {
+  $validation = pathinfo($_FILES["json"]["name"]);
+  if (@$validation['extension'] === 'json') {
+    move_uploaded_file($_FILES["json"]["tmp_name"], "tests/".$_FILES["json"]["name"]);
+    header("Location: list.php");
+  } elseif (@is_null($validation['extension'])) {
+    echo '<h3>Вы не выбрали файл!<h3><br />';
+  } else {
+  echo '<h3>Можно загрузить только файл в формате .json<h3><br />';
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -10,52 +23,36 @@ $files = array_diff( scandir( $dir), array('..', '.'));
     <title>Страница загрузки файлов с тестами</title>
 </head>
 <body>
-	<h3>Выберите файл для загрузки (.json)</h3>
-  <?php echo '<pre>
-  Пример структуры файла test_1.json:
+  	<h3>Вы можете добавить файл с тестом на сервер</h3>
+    <p>(поддерживаются только файлы с расширением .json)</p><br/>
+    <h3>Пример структуры файла test_1.json:</h3>
+    <pre>
 
+  {
+  "question": "СКОЛЬКО ДНЕЙ В ГОДУ?",
+  "answers": [
+    1024,
+    512,
+    365,
+    256
+  ],
+  "correct": 365
+  }
 
-    {
-    "question": "СКОЛЬКО ДНЕЙ В ГОДУ?",
-    "answers": [
-      1024,
-      512,
-      365,
-      256
-    ],
-    "correct": 365
-    }
-
-    
-  </pre>'; ?>
-
-	<form action="admin.php" method="POST" enctype="multipart/form-data">
-    <input type="file" name="json"><br /><br />
-    <input type="reset" value="Очистить">
-    <input type="submit" value="Отправить"><br /><br /><br />
-  </form>
-  <?php
-      if (!empty($_FILES["json"])) {
-        $validation = pathinfo($_FILES["json"]["name"]);
-        if (@$validation['extension'] === 'json') {
-          move_uploaded_file($_FILES["json"]["tmp_name"], "tests/".$_FILES["json"]["name"]);
-          header("Location: list.php");
-        } elseif (@is_null($validation['extension'])) {
-          echo '<h3>Вы не выбрали файл!<h3><br />';
-        } else {
-        echo '<h3>Можно загрузить только файл в формате .json<h3><br />';
-        }
-      }
-  ?>
-  <h3>Список загруженных тестов:</h3>
-  <?php 
-    foreach ($files as $key => $value) {
+    </pre>
+  	<form action="admin.php" method="POST" enctype="multipart/form-data">
+      <input type="file" name="json"><br/><br/>
+      <input type="reset" value="Очистить">
+      <input type="submit" value="Отправить"><br/><br/><br/>
+    </form>
+    <h3>Список загруженных тестов:</h3>
+      <?php foreach ($files as $key => $value) {
         $num = $key - 1;
         echo "$num. $value";
         echo '<br />';
-    }
-    echo '<br /><br /><br /><br />';
-  ?>
-  <a href="list.php"><button>Перейти к выбору теста</button></a>
+      }
+      echo '<br /><br /><br /><br />';
+      ?>
+    <a href="list.php"><button>Перейти к выбору теста</button></a>
 </body>
 </html>
